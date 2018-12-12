96c475d8c458781539dee0a5121b33d872bd1da6
Nofar Oshry <noshry@campus.haifa.ac.il>
2018-12-12 21:35:51 +0200

    edits

--- app/src/main/java/com/nastia/cf/addNutrition.java
+++ app/src/main/java/com/nastia/cf/addNutrition.java
@@ -1,12 +1,17 @@
 package com.nastia.cf;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,7 +30,10 @@ public class addNutrition extends AppCompatActivity {
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-    Spinner spinner2;
+    Spinner spinner2;   //food type
+    Spinner spinner3;   //unit of measurement
+    TextView amount;
+    Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,56 @@ public class addNutrition extends AppCompatActivity {
         setContentView(R.layout.activity_add_nutrition);
 
         spinner2=(Spinner)findViewById(R.id.spinner2);
+        spinner3=(Spinner)findViewById(R.id.spinner3);
+        amount=(TextView)findViewById(R.id.editText2);
+        saveBtn=(Button)findViewById(R.id.saveBtn);
+
+        // adapter for food type spinner
+        @SuppressLint("ResourceType") ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.foods_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // adapter for unit of measurement spinner
+        @SuppressLint("ResourceType") ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                this, R.array.unit_of_measure, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner3.setAdapter(adapter2);
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(addNutrition.this,NutritionActivity.class));
+            }
+        });
       /*  ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
 
@@ -61,16 +119,5 @@ public class addNutrition extends AppCompatActivity {
                         }
                     }
                 });
-
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        @SuppressLint("ResourceType") ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(
-                this, android.R.layout.simple_spinner_item, spinnerArray);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner2.setAdapter(adapter);
-
-
     }
 }

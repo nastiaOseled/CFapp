8bdb31867cae9d3c42a5ecc9483480101d86e47f
Nofar Oshry <noshry@campus.haifa.ac.il>
2018-12-15 18:18:35 +0200

    edits

--- app/src/main/java/com/nastia/cf/addNutrition.java
+++ app/src/main/java/com/nastia/cf/addNutrition.java
@@ -1,30 +1,45 @@
 package com.nastia.cf;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class addNutrition extends AppCompatActivity {
 
+    private static final int CONVERT_G_TO_UNIT = 3;
+    private static final int CONVERT_G_TO_CUP = 2;
     private static final String TAG = addNutrition.class.getSimpleName();
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -45,17 +60,16 @@ public class addNutrition extends AppCompatActivity {
         amount=(TextView)findViewById(R.id.editText2);
         saveBtn=(Button)findViewById(R.id.saveBtn);
 
-        // adapter for food type spinner
-        @SuppressLint("ResourceType") ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this, R.array.foods_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner2.setAdapter(adapter);
+        setComboBox();
+
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                Toast.makeText(parent.getContext(),
+                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_LONG).show();
+                if(parent.getItemAtPosition(position).toString().equals("אחר"))
+                        getInputFromUser();
             }
 
             @Override
@@ -67,35 +81,58 @@ public class addNutrition extends AppCompatActivity {
         // adapter for unit of measurement spinner
         @SuppressLint("ResourceType") ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
                 this, R.array.unit_of_measure, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter2);
-        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                DocumentSnapshot document;
+                String anotherFood=spinner2.getSelectedItem().toString();
+                DocumentReference user_details = db.collection("Nutrition")
+                        .document(anotherFood);
 
+                user_details.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            long calories=0;
+                            if (document.exists())
+                                calories=document.getLong("calories");
+                            int caloriesToInsert=0;
+                            int amount2=Integer.parseInt(amount.getText().toString());
+                            String anotherFood=spinner2.getSelectedItem().toString();
+                            Map<String, Object> newFood = new HashMap<>();
+                            switch(spinner3.getSelectedItem().toString()){
+                                case  "כוס" :
+                                    caloriesToInsert=(int) calories *CONVERT_G_TO_CUP*amount2;
+                                    newFood.put("name", anotherFood);
+                                    newFood.put("calories", caloriesToInsert);
+                                    break;
 
+                                case  "יחידה" :
+                                    caloriesToInsert=(int) calories *CONVERT_G_TO_UNIT*amount2;
+                                    newFood.put("name", anotherFood);
+                                    newFood.put("calories", caloriesToInsert);
+                                    break;
+
+                                default:
+                                    caloriesToInsert=(int) calories *amount2;
+                                    newFood.put("name", anotherFood);
+                                    newFood.put("calories", caloriesToInsert);
+                                    break;
+                            }
+
+                            //insert new food to user's nutrition reports list
+                            insertNewFoodToUserNutritionList(anotherFood, newFood);
+                        } }});
                 startActivity(new Intent(addNutrition.this,NutritionActivity.class));
             }
-        });
-      /*  ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter); */
+        });
+
     }
 
 
@@ -113,10 +150,100 @@ public class addNutrition extends AppCompatActivity {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
                                 spinnerArray.add(document.getString("name"));
                             }
-                            spinnerArray.add("other");
+                            spinnerArray.add("אחר");
+
+                            //set adapter
+                              ArrayAdapter<CharSequence> adapter = new ArrayAdapter(addNutrition.this,
+                              android.R.layout.simple_spinner_item, spinnerArray);
+                                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                 if(spinnerArray.isEmpty())
+                                    Log.i("","array is empty");
+                                 else
+                                     Log.i("","array is not empty");
+                                  spinner2.setAdapter(adapter);
+
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
+                    }
+                });
+    }
+
+    public void getInputFromUser(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("נא הכנס סוג מזון");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_NORMAL);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("אישור", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String anotherFood = input.getText().toString();
+
+                //set the document before insert
+                Map<String, Object> newFood = new HashMap<>();
+                newFood.put("name", anotherFood);
+                newFood.put("calories", 100);
+                newFood.put("unit", "100 גרם");
+
+                //insert new value to nutrition list
+                insertNewValueToNutritionCollection(anotherFood, newFood);
+
+                //insert new value to user's nutrition reports list
+                insertNewFoodToUserNutritionList(anotherFood, newFood);
+
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("ביטול", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void insertNewValueToNutritionCollection(String anotherFood, Map<String, Object> newFood){
+        db.collection("Nutrition").document(anotherFood)
+                .set(newFood)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+            }
+
+
+    public void insertNewFoodToUserNutritionList(String anotherFood, Map<String, Object> newFood){
+        final DocumentReference user_details = db.collection("user_details")
+                .document(mAuth.getCurrentUser().getUid());
+
+        user_details.collection("nutrition reports").document(anotherFood)
+                .set(newFood)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written to user!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document to user", e);
                     }
                 });
     }
